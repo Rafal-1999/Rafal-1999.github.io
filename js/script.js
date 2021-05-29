1b56fe5bdf6cbb1212e2f1fc656786df6c3c90ab
Rafal-1999 <skapinka1999@wp.pl>
2021-05-29 15:20:24 +0200

    Replaced script

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Skrypt na ukrycie przycisku powrotu do gór w momencie gdy jest się na samej górze.
+let scrolling = document.getElementsByClassName("container-icon__arrow-up");
+function scrollFunction() {
+    if (window.scrollY > 20) {
+        console.log(scrollY);
+        // document.getElementsByClassName(".container-icon__arrow-up").style.display = "block";
+        scrolling.style.display = "block";
+        console.log("Pokaż go");
+    } else {
+        // document.getElementsByClassName(".container-icon__arrow-up").style.display = "none";
+        scrolling.style.display = "none";
+    }
+}
+function scrollFunctionUp() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = () => {
+    scrollFunction();
+}
